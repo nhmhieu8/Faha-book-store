c96feeca0778a38660a10a7cfc1a1df116c15722
Hieu Nguyen <nhmhieu8@gmail.com>
2024-04-11 13:14:05 +0700

    design api for home products and get all products

---
+++ Frontend/apiDesign.ts
@@ -0,0 +1,68 @@
+export const HomeProducts = [
+   {
+      productType: "Sách tiếng việt",
+      productList: [
+         {
+            productId: 1,
+            imgSrc:
+               "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            title: "Thiếu nhi 1",
+            price: "500.000",
+            subtype: "Thiếu nhi",
+         },
+         {
+            productId: 2,
+            imgSrc:
+               "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            title: "Văn học 2",
+            price: "500.000",
+            subtype: "Văn học",
+         },
+      ],
+   },
+];
+
+export const allProducts = [
+   {
+      productId: 1,
+      imgSrc:
+         "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      title: "Thiếu nhi 1",
+      price: "500.000",
+      //for type "Sách tiếng việt"
+      productType: "Sách tiếng việt",
+      genre: "Thiếu nhi",
+      publisher: "NXB Kim Đồng",
+      language: "Tiếng việt",
+      coverForm: "Bìa mềm",
+      // ...
+   },
+   {
+      productId: 2,
+      imgSrc:
+         "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      title: "Thiếu nhi 1",
+      price: "500.000",
+      //for type "foreign book"
+      productType: "foreign book",
+      genre: "Children",
+      publisher: "Cambridge Press",
+      language: "English",
+      coverForm: "paperback",
+      // ...
+   },
+   {
+      productId: 3,
+      imgSrc:
+         "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      title: "Bút Thiên Long",
+      price: "3.000",
+      //for type "stationery"
+      productType: "Văn phòng phẩm",
+      subtype: "Bút - viết",
+      origin: "Việt Nam",
+      color: "cam gỗ",
+      inkColor: "đen",
+      // ...
+   },
+];
